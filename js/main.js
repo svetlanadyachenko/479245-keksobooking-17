3d3e8d362b9135903e1f790de3fde114e03beda5
Sveta <angelinalana84@gmail.com>
2019-06-18 14:44:34 +0300

    Исправление кода в соответствии с критериями

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 'use strict';
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var AD_QUANTITY = 8;
+var AD_WIDTH = 50;
+
+var mapElement = document.querySelector('.map');
+mapElement.classList.remove('map--faded');
 
 var similarListElement = document.querySelector('.map__pins');
 var similarAdTemplate = document.querySelector('#pin')
 .content
 .querySelector('.map__pin');
+
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-var AVATARS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var AD_QUANTITY = 8;
-var AD_WIDTH = 50;
-
 var getRandomElement = function (array) {
-  return array[Math.floor(Math.random() * array.length)];
+  return array[(getRandomNumber() * array.length)];
 };
 
 var getAdsData = function (quantity) {
   var ads = [];
   for (var i = 0; i < quantity; i++) {
-    var tag = {
-      author: getRandomElement(AVATARS),
+    var ad = {
+      author: 'img/avatars/user0' + (i + 1) + '.png',
       offer: getRandomElement(TYPES),
       location: {
         x: getRandomNumber(0, 1200),
         y: getRandomNumber(130, 630)
       }
     };
-    ads[i] = tag;
+    ads[i] = ad;
   }
   return ads;
 };
@@ -41,17 +41,18 @@ var ads = getAdsData(AD_QUANTITY);
 var renderAd = function (ad) {
   var adElement = similarAdTemplate.cloneNode(true);
 
-  adElement.style = 'left' + ':' + ' ' + (ad.location.x + (AD_WIDTH / 2)) + 'px' + ';' + ' ' + 'top' + ':' + ' ' + ad.location.y + 'px' + ';';
+  adElement.style.left = ad.location.x + (AD_WIDTH / 2) + 'px';
+  adElement.style.top = ad.location.y + 'px';
   adElement.querySelector('.map__pin img').src = ad.author;
   adElement.querySelector('.map__pin img').alt = ad.offer;
 
   return adElement;
 };
 
-var getFragment = function (tags) {
+var getFragment = function (data) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < tags.length; i++) {
-    similarListElement.appendChild(renderAd(tags[i]));
+  for (var i = 0; i < data.length; i++) {
+    fragment.appendChild(renderAd(data[i]));
   }
   return fragment;
 };
