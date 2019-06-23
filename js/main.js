9b2811e2a2f7a62c44d30a166a0d98f30786ad2c
Sveta <angelinalana84@gmail.com>
2019-06-23 20:13:11 +0300

    Задание Личный проект: подробности

--- js/main.js
+++ js/main.js
@@ -9,9 +9,6 @@ var X_LAST_COORDINATE = 1200;
 var Y_FIRST_COORDINATE = 130;
 var Y_LAST_COORDINATE = 630;
 
-var mapElement = document.querySelector('.map');
-mapElement.classList.remove('map--faded');
-
 var similarListElement = document.querySelector('.map__pins');
 var similarAdTemplate = document.querySelector('#pin')
 .content
@@ -47,9 +44,10 @@ var renderAd = function (ad) {
   var adElement = similarAdTemplate.cloneNode(true);
 
   adElement.style.left = ad.location.x - (AD_WIDTH / 2) + 'px';
-  adElement.style.top = ad.location.y - (AD_HEIGHT / 2) + 'px';
+  adElement.style.top = ad.location.y - AD_HEIGHT + 'px';
   adElement.querySelector('.map__pin img').src = ad.author;
   adElement.querySelector('.map__pin img').alt = ad.offer;
+  adElement.setAttribute('class', 'map__pin pin');
 
   return adElement;
 };
@@ -62,4 +60,64 @@ var getFragment = function (data) {
   return fragment;
 };
 
-similarListElement.appendChild(getFragment(ads));
+var mapElement = document.querySelector('.map');
+mapElement.classList.add('map--faded');
+var mapPinMain = document.querySelector('.map__pin--main');
+var adForm = document.querySelector('.ad-form');
+adForm.classList.add('ad-form--disabled');
+var fieldsetInAdForm = adForm.querySelectorAll('fieldset');
+var filtersForm = mapElement.querySelector('.map__filters');
+var filtersSelect = filtersForm.querySelectorAll('select');
+var resetButton = document.querySelector('.ad-form__reset');
+var mapPinMainXPosition = 570;
+var mapPinMainYPosition = 375;
+var addressInput = adForm.querySelector('input[name="address"]');
+
+var getDisabledElements = function (elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].setAttribute('disabled', 'disabled');
+  }
+  return elements;
+};
+
+var removeDisabledElements = function (elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].removeAttribute('disabled', 'disabled');
+  }
+  return elements;
+};
+
+getDisabledElements(fieldsetInAdForm);
+getDisabledElements(filtersSelect);
+
+
+var getActiveMap = function () {
+  mapElement.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+  removeDisabledElements(fieldsetInAdForm);
+  removeDisabledElements(filtersSelect);
+  similarListElement.appendChild(getFragment(ads));
+};
+
+var getDisabledMap = function () {
+  mapElement.classList.add('map--faded');
+  adForm.classList.add('ad-form--disabled');
+  getDisabledElements(fieldsetInAdForm);
+  getDisabledElements(filtersSelect);
+  var pins = document.querySelectorAll('.pin');
+  for (var i = pins.length - 1; i >= 0; i--) {
+    var pin = pins[i];
+    pin.parentElement.removeChild(pin);
+  }
+};
+
+var mapPinMainPosition = mapPinMainXPosition + ', ' + mapPinMainYPosition;
+
+mapPinMain.addEventListener('click', function () {
+  getActiveMap();
+  addressInput.value = mapPinMainPosition;
+});
+
+resetButton.addEventListener('click', function () {
+  getDisabledMap();
+});
